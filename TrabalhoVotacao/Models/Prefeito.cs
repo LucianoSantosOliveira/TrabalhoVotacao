01eb4fec0d22bf5aa888ec763f2f871049e26237
LucianoSantosOliveira <lucianoliv2@hotmail.com>
2022-11-29 19:47:13 -0300

    Adicionar arquivos de projeto.

---
+++ TrabalhoVotacao/Models/Prefeito.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoVotacao.Models
+{
+    public class Prefeito
+    {
+        public Prefeito() { }
+        public Guid Id { get; set; }
+        public string nome { get; set; }
+        public int QuantidadeDeVotos { get; set; }
+        public string Partido { get; set; }
+    }
+}
---
+++ TrabalhoVotacao/Program.cs
@@ -0,0 +1,50 @@
+﻿using TrabalhoVotacao.Models.Eleicoes;
+
+Console.WriteLine("Hello, World!");
+var opcao = Console.ReadLine();
+
+switch (opcao)
+{
+    case "1":
+        ModuloExecutivo();
+        break;
+    case "2":
+        ModuloLegislativo();
+        break ;
+    default:
+        break;
+}
+
+void ModuloLegislativo()
+{
+    Console.Clear();
+    var eleicaoLegislativo = new EleicaoLegislativo();
+    Console.WriteLine("Modulo legislativo");
+    var opcao = Console.ReadLine();
+    switch (opcao)
+    {
+        case "1":
+            Console.WriteLine("Eleições para Vereador");
+            break;
+        default:
+            break;
+    }
+    Console.ReadLine();
+};
+
+void ModuloExecutivo()
+{
+    Console.Clear();
+    var eleicaoExecutivo = new EleicaoExecutivo();
+    Console.WriteLine("Modulo legislativo");
+    var opcao = Console.ReadLine();
+    switch (opcao)
+    {
+        case "1":
+            Console.WriteLine("Eleições para Prefeito");
+            break;
+        default:
+            break;
+    }
+    Console.ReadLine();
+};
